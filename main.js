07cff0eff9e9c51783ebf81c3810dd81290d4daa
Kairuihu2903 <78373343+Kairuihu2903@users.noreply.github.com>
2022-10-27 18:14:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1b9045ab5dmsh178dd0dccefee7ap1db6e3jsna21cc9383b5c',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
+  method: 'GET',
+  url: 'https://imdb8.p.rapidapi.com/auto-complete',
+  params: {q: 'game of throne'},
+  headers: {
+    'X-RapidAPI-Key': 'e5211403d0msh294e51f5227f293p153424jsnc09534c8a754',
+    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+  }
 };
 
-fetch('https://imdb8.p.rapidapi.com/title/get-genres?tconst=tt0944947', options)
+axios?.request(options)
+	.then(function (response) {console.log(response.data);})
+	.then(data => {
+		const list = data.d;
+		list?.map((item) => {
+			const name = item.l;
+			const poster = item.i.imageUrl;
+			const movie = `<li><img ssrc ="${poster}> <h2>${name}</h2></li>`
+			document.querySelector('.movies').innerHTML += movie;
+		})
+	.catch(function (error) {console.error(error);});
+});
+//////////////////////////////////////// will fetch from url q=...// allspaces need to be changed to%20
+
+/*
+fetch('https://imdb8.p.rapidapi.com/title/find?q=game%20of%20thr', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(data => {
+		const list = data.d;
+
+		list?.map((item) => {
+			const name = item.l;
+			const poster = item.i.imageUrl;
+			const movie = `<li><img ssrc ="${poster}> <h2>${name}</h2></li>`
+			document.querySelector('.movies').innerHTML += movie;
+		})
+	})
+	.catch(err => {
+		console.error(err);
+	});
+
+
+*/
